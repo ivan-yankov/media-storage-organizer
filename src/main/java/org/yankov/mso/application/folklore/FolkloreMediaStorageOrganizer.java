588a83e89dc189787b4fa601f42a064c95a474df
Ivan Yankov <ivan.iankov@gmail.com>
2017-12-03 20:27:58 +0200

    Setup JavaFX

---
+++ src/main/java/org/yankov/mso/application/folklore/FolkloreMediaStorageOrganizer.java
@@ -0,0 +1,17 @@
+package org.yankov.mso.application.folklore;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FolkloreMediaStorageOrganizer extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
