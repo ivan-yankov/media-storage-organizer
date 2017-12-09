6e5fb2177a45ec8742a507d6eee8df9ca6d4e167
Ivan Yankov <ivan.iankov@gmail.com>
2017-12-09 23:49:00 +0200

    Create jar file on build (add missing file)

---
+++ src/main/java/org/yankov/mso/application/generic/ApplicationSettings.java
@@ -0,0 +1,26 @@
+package org.yankov.mso.application.generic;
+
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+import java.util.Optional;
+
+public interface ApplicationSettings {
+
+    boolean isMaximized();
+
+    Optional<Image> getIcon();
+
+    Optional<String> getTitle();
+
+    Optional<Double> getWindowWidth();
+
+    Optional<Double> getWindowHeight();
+
+    Optional<Double> getX();
+
+    Optional<Double> getY();
+
+    Scene getScene();
+
+}
