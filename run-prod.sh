a81d86fff8c23f8517154de39d805ecba6ff848b
Ivan <ivan.iankov@gmail.com>
2020-12-06 22:39:17 +0200

    Scripts

--- run-prod.sh
+++ run-prod.sh
@@ -1,5 +1,5 @@
 VERSION='1.9'
-DB=/media/ivan-yankov/9df430e5-1b64-45cf-8c51-42d3d37e89fe/folklore-media-storage-organizer/database
+DB=../folklore-media-storage-organizer/database
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export JRE_HOME=$JAVA_HOME/jre
