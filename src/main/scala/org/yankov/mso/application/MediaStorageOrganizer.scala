c4a428267cf925aa0f8b70e5f9610097f7223684
Ivan <ivan.iankov@gmail.com>
2020-12-07 10:21:43 +0200

    Setup ScalaFX project

---
+++ src/main/scala/org/yankov/mso/application/MediaStorageOrganizer.scala
@@ -0,0 +1,20 @@
+package org.yankov.mso.application
+
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.control.Label
+import scalafx.scene.layout.BorderPane
+
+object MediaStorageOrganizer extends JFXApp {
+  stage = new PrimaryStage {
+    title = "Hello"
+    scene = new Scene {
+      root = new BorderPane {
+        padding = Insets(25)
+        center = new Label("Hello World")
+      }
+    }
+  }
+}
