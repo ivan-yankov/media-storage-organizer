c52483b8da36f6c8b14e016d2025f2ef2ad38922
Ivan <ivan.iankov@gmail.com>
2020-12-14 08:03:03 +0200

    Infrastructure improvements and fixes

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+source var.sh
+cp target/scala-2.12/media-storage-organizer-assembly-2.0.jar $PROD_DIR
--- run-prod.sh
+++ run-prod.sh
@@ -1,4 +1,3 @@
-VERSION='2.0'
-DB=../folklore-media-storage-organizer/database
+source var.sh
 
-java -Xms2G -Xmx6G -jar target/scala-2.12/media-storage-organizer-assembly-$VERSION.jar #--db-url jdbc:derby:$DB
+java -Xms2G -Xmx6G -jar target/scala-2.12/media-storage-organizer-assembly-$VERSION.jar $PROD_DIR/database
