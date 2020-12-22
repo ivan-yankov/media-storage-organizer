2da50bb7bcd91b45e6dec6bccfba0b6e932c3b85
Ivan <ivan.iankov@gmail.com>
2020-12-23 00:14:22 +0200

    Scripts

--- run-dev.sh
+++ run-dev.sh
@@ -1,4 +1,2 @@
-VERSION='2.0'
-DB=testdb
-
-java -Xms2G -Xmx6G -jar target/scala-2.12/media-storage-organizer-assembly-$VERSION.jar #--db-url jdbc:derby:$DB
+source var.sh
+java -Xms2G -Xmx6G -jar target/scala-2.12/media-storage-organizer-assembly-$VERSION.jar --db-dir $TEST_DB
