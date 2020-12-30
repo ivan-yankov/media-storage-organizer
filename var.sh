649994c24305fde0b4bd8ba8ad97ed3ce1f88064
Ivan <ivan.iankov@gmail.com>
2020-12-30 16:01:11 +0200

    Fix

--- var.sh
+++ var.sh
@@ -1,6 +1,6 @@
 export VERSION='2.0'
 
-export PROD_DIR=/media/ivan-yankov/9df430e5-1b64-45cf-8c51-42d3d37e89fe/folklore
+export PROD_DIR=/media/ivan-yankov/9df430e5-1b64-45cf-8c51-42d3d37e89fe/folklore/data
 export TEST_DIR=./test-data
 
 export PROD_DB=$PROD_DIR/database
