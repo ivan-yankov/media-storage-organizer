e73442c56a7a5574b60ecce72b2d7747e663e253
Ivan <ivan.iankov@gmail.com>
2021-11-20 18:39:18 +0200

    Tests

--- src/test/scala/org/yankov/mso/application/database/FakeDatabase.scala
+++ src/test/scala/org/yankov/mso/application/database/FakeDatabase.scala
@@ -1,6 +1,7 @@
 package org.yankov.mso.application.database
 
 import io.circe.{Decoder, Encoder}
+import org.yankov.mso.application.Id
 import org.yankov.mso.application.model.DatabaseModel.DbEntry
 
 import java.io.InputStream
@@ -11,10 +12,17 @@ case class FakeDatabase() extends Database {
   private var insertPath: Path = _
   private var insertResult: Either[String, Unit] = _
 
-  private var readKeys: List[String] = _
+  private var readKeys: List[Id] = _
   private var readPath: Path = _
   private var readResult: Either[String, List[_]] = _
 
+  private var updateEntries: List[_] = _
+  private var updatePath: Path = _
+  private var updateResult: Either[String, List[Id]] = _
+
+  private var deleteKeys: List[Id] = _
+  private var deletePath: Path = _
+  private var deleteResult: Either[String, Int] = _
 
   def getInsertEntries: List[_] = insertEntries
 
@@ -22,11 +30,23 @@ case class FakeDatabase() extends Database {
 
   def setInsertResult(result: Either[String, Unit]): Unit = insertResult = result
 
-  def getReadKeys: List[String] = readKeys
+  def getReadKeys: List[Id] = readKeys
 
   def getReadPath: Path = readPath
 
   def setReadResult(result: Either[String, List[_]]): Unit = readResult = result
+
+  def getUpdateEntries: List[_] = updateEntries
+
+  def getUpdatePath: Path = updatePath
+
+  def setUpdateResult(result: Either[String, List[Id]]): Unit = updateResult = result
+
+  def getDeleteKeys: List[Id] = deleteKeys
+
+  def getDeletePath: Path = deletePath
+
+  def setDeleteResult(result: Either[String, Int]): Unit = deleteResult = result
 
   override def insert[T <: DbEntry](entries: List[T], path: Path)
                                    (implicit encoder: Encoder[T]): Either[String, Unit] = {
@@ -35,7 +55,7 @@ case class FakeDatabase() extends Database {
     insertResult
   }
 
-  override def read[T](keys: List[String], path: Path, inputStream: Path => InputStream)
+  override def read[T](keys: List[Id], path: Path, inputStream: Path => InputStream)
                       (implicit decoder: Decoder[T]): Either[String, List[T]] = {
     readKeys = keys
     readPath = path
@@ -46,7 +66,15 @@ case class FakeDatabase() extends Database {
   }
 
   override def update[T <: DbEntry](entries: List[T], path: Path, inputStream: Path => InputStream)
-                                   (implicit encoder: Encoder[T]): Either[String, List[String]] = ???
+                                   (implicit encoder: Encoder[T]): Either[String, List[Id]] = {
+    updateEntries = entries
+    updatePath = path
+    updateResult
+  }
 
-  override def delete(keys: List[String], path: Path): Either[String, Int] = ???
+  override def delete(keys: List[Id], path: Path): Either[String, Int] = {
+    deleteKeys = keys
+    deletePath = path
+    deleteResult
+  }
 }
