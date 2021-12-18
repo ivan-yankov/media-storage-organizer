730b9e92925fac220a619681e6d5300483ceeba2
Ivan <ivan.iankov@gmail.com>
2021-12-18 16:58:11 +0200

    Refactoring

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
 echo "deploy <majorVersion>.<minorVersion> <rootDir>"
-cp target/scala-2.12/media-storage-organizer-assembly-$1.jar $2
+dest=$2
+cp target/scala-2.12/media-storage-organizer-assembly-$1.jar $dest
+cp start.bat $dest
+cp start.sh $dest
