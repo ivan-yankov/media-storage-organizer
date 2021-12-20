9d4c5438d7b0156f81dd88979abc0c0c4a877ce9
Ivan <ivan.iankov@gmail.com>
2021-12-20 11:58:00 +0200

    Update infrastructure

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-echo "deploy <majorVersion>.<minorVersion> <rootDir>"
-dest=$2
-cp target/scala-2.12/media-storage-organizer-assembly-$1.jar $dest
+echo "deploy <rootDir>"
+dest=$1
+cp target/scala-2.12/media-storage-organizer-assembly-3.2.jar $dest
 cp start.bat $dest
 cp start.sh $dest
